75ad79424fbc9911f90ae84d242e9d241d24f817
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2024-04-15 15:36:07 +0200

    Add store example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 // import './style.css'
-import App from './AppTemperature.vue';
-// import App from './AppSchedule.vue';
+// import App from './AppTemperature.vue';
+import App from './AppSchedule.vue';
 
 createApp(App).mount('#app');
